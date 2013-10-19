474015d2416ce823d32acbfd126c8a4f1301cb80
Philipp Frenzel <philipp@frenzel.net>
2013-10-19 15:06:49 +0200

    basic template setup

---
+++ qvCRUD/config/console.php
@@ -0,0 +1,25 @@
+<?php
+$params = require(__DIR__ . '/params.php');
+return [
+	'id' => 'bootstrap-console',
+	'basePath' => dirname(__DIR__),
+	'preload' => ['log'],
+	'controllerPath' => dirname(__DIR__) . '/commands',
+	'controllerNamespace' => 'app\commands',
+	'modules' => [
+	],
+	'components' => [
+		'cache' => [
+			'class' => 'yii\caching\FileCache',
+		],
+		'log' => [
+			'targets' => [
+				[
+					'class' => 'yii\log\FileTarget',
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+	],
+	'params' => $params,
+];
