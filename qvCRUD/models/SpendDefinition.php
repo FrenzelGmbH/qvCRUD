1eedf40853cedeb65d5e52bc8a4ba5e854529c49
Philipp Frenzel <philipp@frenzel.net>
2013-10-30 09:02:37 +0100

    update spenddefinition model attributes

--- qvCRUD/models/SpendDefinition.php
+++ qvCRUD/models/SpendDefinition.php
@@ -10,6 +10,13 @@ use Yii;
  */
 class SpendDefinition extends \yii\db\ActiveRecord
 {
+
+	public $ValidFrom;
+  public $IsValid;
+  public $SpendDefID;
+  public $SpendDefDescription;
+  public $Parent;
+
 	public static function getDb()
 	{
 		return Yii::$app->getComponent('dbsproserv');
@@ -29,7 +36,10 @@ class SpendDefinition extends \yii\db\ActiveRecord
 	public function rules()
 	{
 		return [
-			
+			['SpendDefID','required'],
+			['ValidFrom, IsValid, SpendDefDescription, Parent','safe'],
+			['SpendDefID','string','max' => 6],
+			['SpendDefDescription','string','max'=>25]
 		];
 	}
 
@@ -39,6 +49,11 @@ class SpendDefinition extends \yii\db\ActiveRecord
 	public function attributeLabels()
 	{
 		return [
+			'ValidFrom' => 'Valid From',
+			'IsValid' 	=> 'Is Valid',
+			'SpendDefID'=> 'Spend Definition Id',
+			'SpendDefDescription' => 'Description',
+			'Parent'	  => 'Parent',
 		];
 	}
 }
